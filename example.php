7b106038978f654b6731cf580a187887f65036ce
Jose Bernal <jbernal.web.dev@gmail.com>
2022-07-20 10:48:52 -0700

    Use system-web package as es6 module.

--- example.php
+++ example.php
@@ -4,7 +4,7 @@
         <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
-        <script src="js/SiteLibraries.js"></script>
+        
         <link rel="stylesheet" href="node_modules/@ocdladefense/ors/dist/css/loading.css" />
         <link rel="stylesheet" href="node_modules/@ocdladefense/ors/dist/css/modal.css" />
         <link rel="stylesheet" href="node_modules/@ocdladefense/modal-inline/dist/modal.css" />
