6dd7daa6beca2055677c79e718fd05a16abe4a67
Jose Bernal <jbernal.web.dev@gmail.com>
2022-08-28 21:11:23 -0700

    Additional examples.

--- example.php
+++ example.php
@@ -2,7 +2,8 @@
 <html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+        </script>
 
         
         <link rel="stylesheet" href="node_modules/@ocdladefense/ors/dist/css/loading.css" />
@@ -13,30 +14,45 @@
     <body>
 
 
-    <?php 
-    $cool = "cool >:)";
-    print($cool);
-    
-    ?>
+        <div class="ors-example">
 
+            <div class="synopsis">This is an except from <a target="_new" href="https://libraryofdefense.ocdla.org/DUII_Notebook:Chapter_3">The DUII Trial Notebook, Chapter 3</a>.</div>
 
-        <div>This is an except from <a target="_new" href="https://libraryofdefense.ocdla.org/DUII_Notebook:Chapter_3">The DUII Trial Notebook, Chapter 3</a>.</div>
+            <h3>
+                <b>Section D - Rescheduling</b>
+            </h3>
 
-        <h2 class="mw-customtoggle-8">
-            <span class="mw-headline" id="D._Rescheduling"><b>D. Rescheduling</b></span> <span class="editsection editSectionInactive" style="float:none" id="editSection-8">[<a href="/index.php?title=DUII_Notebook:Chapter_3&amp;action=edit&amp;section=8" class="editSectionLinkInactive" id="editSectionAnchor-8" onclick="MediaWikiModule__EditSectionClearerLink_ExecuteEditLink(event,this);" title="Edit section: D. Rescheduling">edit</a>]<span class="editSectionChromeInactive" id="editSectionChrome-8">⇲</span></span>
-        </h2>
-
-        <div class="editableSection mw-collapsible mw-made-collapsible" id="mw-customcollapsible-8">
+            
             <p>If the need arises to reschedule the hearing date, please note that if the petitioner files the request, the client’s license will be suspended within the time period indicated on the implied consent form. If the officer is unavailable and the hearing is rescheduled, the client’s license ORS 813.440 suspension is withdrawn until the outcome of the hearing. Reasons for allowing the department to reschedule the hearing are covered in ORS 813.440 and includes the inability of the attorney to appear due to the attorney’s illness, vacation or scheduling conflict arising from other court or administrative hearing appearances.
             </p>
+            
+
+            <h3>
+                <b>Section E - Servicemembers</b>
+            </h3>
+
+        
+            <p>If the client is in the military, different rules will apply. Pursuant to ORS 183.413(a), a member of the Armed Forces (Army, Navy, Air Force, Marines, Coast Guard and National Guard) may be subject to the protection of the Servicemembers’ Civil Relief Act (SCRA). The SCRA provides certain protections to servicemembers and their dependents, if specific conditions are met. If your client qualifies, he or she may be entitled to have the license suspension matter stayed. You will need to get a copy of the client's service orders as it pertains to the specific condition.
+            </p>
+            
         </div>
 
-        <h2 class="mw-customtoggle-9">
-            <span class="mw-headline" id="E._Servicemembers"><b>E. Servicemembers</b></span> <span class="editsection editSectionInactive" style="float:none" id="editSection-9">[<a href="/index.php?title=DUII_Notebook:Chapter_3&amp;action=edit&amp;section=9" class="editSectionLinkInactive" id="editSectionAnchor-9" onclick="MediaWikiModule__EditSectionClearerLink_ExecuteEditLink(event,this);" title="Edit section: E. Servicemembers">edit</a>]<span class="editSectionChromeInactive" id="editSectionChrome-9">⇲</span></span>
-        </h2>
 
-        <div class="editableSection mw-collapsible mw-made-collapsible" id="mw-customcollapsible-9">
-            <p>If the client is in the military, different rules will apply. Pursuant to ORS 183.413(a), a member of the Armed Forces (Army, Navy, Air Force, Marines, Coast Guard and National Guard) may be subject to the protection of the Servicemembers’ Civil Relief Act (SCRA). The SCRA provides certain protections to servicemembers and their dependents, if specific conditions are met. If your client qualifies, he or she may be entitled to have the license suspension matter stayed. You will need to get a copy of the client’s service orders as it pertains to the specific condition.
+
+        <div class="ors-example">
+
+            <div class="synopsis">This is an except from <a target="_new" href="https://libraryofdefense.ocdla.org/Search_and_Seizure:Chapter_1B_Search">Search and Seizure: Chapter 1B - Search</a>.</div>
+
+        
+            <h3>Section F - Invasions of Privacy Interests by Listening</h3>
+
+            <p>Individuals have a cognizable privacy interest in the content of their telephone calls. <i>See</i> <a rel="nofollow" class="external text" href="http://www.oregonlaws.org/ors/133.724">ORS 133.724</a> (setting out requirement that police obtain judicially issued warrant to intercept a telephonic communication). Listening to a cordless telephone conversation on a “scanner” is a search. <a rel="nofollow" class="external text" href="http://scholar.google.com/scholar_case?q=State+v.+Carston/+Sage/+Sage/+Gralla,+323+Or+75,+&amp;hl=en&amp;as_sdt=4,38&amp;case=7250644545103351989&amp;scilh=0"><i>State v. Carston/ Sage/ Sage/ Gralla</i>, 323 Or 75, 913 P2d 709 (1996)</a>. However, a person does not have a privacy interest in records that are kept by a third party that do not reveal the contents of a conversation. <a rel="nofollow" class="external text" href="http://scholar.google.com/scholar_case?q=State+v.+Johnson,+340+Or+319,+&amp;hl=en&amp;as_sdt=4,38&amp;case=16302940737552748233&amp;scilh=0"><i>State v. Johnson</i>, 340 Or 319, 336, 131 P3d 173, 184 (2006)</a> <i>cert den,</i> 549 US 1079 (2006) (“[W]e cannot identify a source of law that establishes that defendant also had some interest in keeping private any records kept by a third party, his cellular telephone provider, respecting his cellular telephone usage.”).
+            </p>
+
+            <p>Unless exigent circumstances are present, a court order is required to record a conversation between an undercover police officer or an informant and a suspect. <a rel="nofollow" class="external text" href="http://scholar.google.com/scholar_case?q=State+v.+Miskell,+351+Or+680,&amp;hl=en&amp;as_sdt=4,38&amp;case=16123622206201009497&amp;scilh=0"><i>State v. Miskell</i>, 351 Or 680, 697-98 277 P3d 522 (2012)</a> (citing US Const, Amend IV; Or Const Art 1, § 9; ORS 133.726(7)(b)) (“We conclude that, in <a rel="nofollow" class="external text" href="http://www.oregonlaws.org/ors/133.726">ORS 133.726(7)(b),</a> the phrase ‘circumstances of such exigency that it would be unreasonable to obtain a court order,’ refers to ‘exigent circumstances’ in the specialized legal sense—as it has been used by this court in discussing exceptions to the search warrant requirement under Article I, section 9, and by the federal courts in discussing exceptions to the warrant requirement under the Fourth Amendment.”). However, if a person is informed that they are being recorded no privacy interest is violated. <a rel="nofollow" class="external text" href="http://scholar.google.com/scholar_case?q=State+v.+Wischnofske,+129+Or+App+231&amp;hl=en&amp;as_sdt=4,38&amp;case=17487738855132574811&amp;scilh=0"><i>State v. Wischnofske</i>, 129 Or App 231, 236, 878 P2d 1130 (1994)</a> (the defendant had no privacy interests in statements he made while he was seated alone in back seat of patrol car following his arrest and, thus, tape-recording statements was not a “search” within meaning of State Constitution’s protection against unreasonable searches and seizures; defendant had been arrested and told that his conversations would be recorded and tape recorder was visible on passenger’s side of front seat).
+            </p>
+
+            <p>Testimonial responses to field sobriety tests are the product of a constitutionally significant search. <a rel="nofollow" class="external text" href="http://scholar.google.com/scholar_case?q=147+Or+App+469&amp;hl=en&amp;as_sdt=4,38&amp;case=6738511133818205236&amp;scilh=0"><i>State v. Gile</i>, 147 Or App 469, 936 P2d 1008 (1997)</a>.
             </p>
         </div>
 
@@ -67,8 +83,8 @@
 
 
 
-       <script type="module" src="js/init.js">
-        </script> 
+    <script type="module" src="js/init.js">
+    </script> 
         
 
 
