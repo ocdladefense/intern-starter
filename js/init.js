ec7e3ada75aa2423cf2ae0efd77c523b6bef63bd
Christian <collisc@my.lanecc.edu>
2022-09-09 11:07:49 -0700

    Refactored and formatted code in chapter.js and init.js. Added XML loading and parsing to chapter.js

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
 import { OrsModal } from "../node_modules/@ocdladefense/ors/dist/modal.js";
 import { OrsParser } from "../node_modules/@ocdladefense/ors/dist/ors-parser.js";
-import {InlineModal} from "../node_modules/@ocdladefense/modal-inline/dist/modal.js";
-import {domReady} from "../node_modules/@ocdladefense/system-web/SiteLibraries.js";
-import {OrsChapter} from "../node_modules/@ocdladefense/ors/src/chapter.js"
+import { InlineModal } from "../node_modules/@ocdladefense/modal-inline/dist/modal.js";
+import { domReady } from "../node_modules/@ocdladefense/system-web/SiteLibraries.js";
+import { OrsChapter } from "../node_modules/@ocdladefense/ors/src/chapter.js"
 
 // List for ORS-related requests.
 document.addEventListener("click", displayOrs);
@@ -14,7 +14,7 @@ let inlineModalFired = false;
 
 
 // Convert the document to be ORS-ready.
-domReady(function() {
+domReady(function () {
 
     convert();
 
@@ -26,65 +26,63 @@ domReady(function() {
 
     const background = document.getElementById("modal-backdrop");
 
-    background.addEventListener("click", function(e) {
-        
+    background.addEventListener("click", function (e) {
+
         let id = e.target.id;
-        if(id != "modal-backdrop")
-        {
+        if (id != "modal-backdrop") {
             return;
         }
 
 
         modal.hide();
     });
-    
+
     const serializer = new XMLSerializer();
 
     let modalTarget = window.modalJr.getRoot();
-    
 
-    
-    let mouseOutCb = getMouseLeaveCallback(modalTarget, function() { window.modalJr.hide(); });
-    let mouseOverCb = getMouseOverCallback(function(x,y,chapter,section){
-        console.log("X coord is ",x);
-        console.log("Y coord is ",y);
-        console.log("Chapter is: ",chapter);
-        console.log("Section is: ",section);
+
+
+    let mouseOutCb = getMouseLeaveCallback(modalTarget, function () { window.modalJr.hide(); });
+    let mouseOverCb = getMouseOverCallback(function (x, y, chapter, section) {
+        console.log("X coord is ", x);
+        console.log("Y coord is ", y);
+        console.log("Chapter is: ", chapter);
+        console.log("Section is: ", section);
 
         // If the modal is already being setup then
         // don't re-initialize.
-        if(inlineModalFired == true)
-        {
+        if (inlineModalFired == true) {
             return false;
         }
         inlineModalFired = true;
 
 
-        window.modalJr.show(x,y);
+        window.modalJr.show(x, y);
 
 
-        let chapterDoc = OrsChapter.getCached(chapter) || new OrsChapter(chapter); 
-        
-        
+        let chapterDoc = OrsChapter.getCached(chapter) || new OrsChapter(chapter);
 
-        
-        chapterDoc.load().then(function(){              
+
+
+
+        chapterDoc.load().then(function () {
             let endSection = chapterDoc.getNextSection(section);
             let cloned = chapterDoc.clone(section, endSection.id);
             let clonedHtml = serializer.serializeToString(cloned);
             window.modalJr.renderHtml(clonedHtml);
             inlineModalFired = false;
         });
-          
-        
+
+
     });
-    
+
 
     modalTarget.addEventListener("mouseleave", mouseOutCb);
 
     let links = document.querySelectorAll("a[data-action]");
 
-    for(var i = 0; i<links.length; i++) {
+    for (var i = 0; i < links.length; i++) {
         links[i].addEventListener("mouseenter", mouseOverCb);
         links[i].addEventListener("mouseleave", mouseOutCb);
     }
@@ -95,10 +93,9 @@ domReady(function() {
 
 function convert() {
     var body = document.querySelector("body");
-    
+
     var text = body.innerHTML;
     var parsed = OrsParser.replaceAll(text);
-    // console.log(parsed);
 
     body.innerHTML = parsed;
 }
@@ -111,7 +108,7 @@ function displayOrs(e) {
     let action = target.dataset && target.dataset.action;
 
     // If we aren't showing an ORS then bail.
-    if(["show-ors"].indexOf(action) === -1) return false;
+    if (["show-ors"].indexOf(action) === -1) return false;
 
     // e.preventDefault();
     // e.stopPropagation();
@@ -124,38 +121,18 @@ function displayOrs(e) {
 
     ors(chapterNum, sectionNum);
 
+
     return false;
 }
 
 
 function ors(chapter, section) {
-     
-    // Network call.
-    //let network = fetchOrs(chapter,section);
-    /*
-    let chapterDoc = new OrsChapter(chapter);
-    chapterDoc.load().then(function (){
-        
-        chapterDoc.injectAnchors();
-        let endSection = chapterDoc.getNextSection(section);
-        chapterDoc.highlight(section, endSection.id);
 
-        let content = chapterDoc.toString();
-        //modal.renderHtml(content);
-        //modal.show();
-
-        let toc = chapterDoc.buildToC();
-        let vols = chapterDoc.buildVolumes();
-        modal.show();
-        modal.toc(toc);
-        modal.titleBar(vols);
-        modal.renderHtml(chapterDoc.toString(), "ors-statutes");
-    });
-    */
     let chapterDoc = OrsChapter.getCached(chapter) || new OrsChapter(chapter);
-    chapterDoc.load().then(function() {
+    chapterDoc.load().then(function () {
         let toc = chapterDoc.buildToc();
         let vols = chapterDoc.buildVolumes();
+        chapterDoc.loadXml();
         modal.show();
         modal.toc(toc);
         modal.titleBar(vols);
@@ -166,7 +143,7 @@ function ors(chapter, section) {
 function getMouseOverCallback(fn) {
 
 
-    return (function(e) {
+    return (function (e) {
         let target = e.target;
         //console.log(e);
 
@@ -176,53 +153,52 @@ function getMouseOverCallback(fn) {
 
         //need to fix this, doesnt work right
         let x = recW + (rectangle.width - e.pageX);
-        if (x > 1575)
-        {
+        if (x > 1575) {
             x = 1575;
         }
         let y = e.pageY;
-        fn(e.pageX-50,e.pageY+1,target.dataset.chapter,target.dataset.section);
+        fn(e.pageX - 50, e.pageY + 1, target.dataset.chapter, target.dataset.section);
 
     });
 }
 
 
 function getMouseLeaveCallback(compareNode, fn) {
-    return (function(e) {
+    return (function (e) {
         let relatedTarget = e.relatedTarget;
         let areTheyEqual = compareNode == relatedTarget;
         console.log("Leave");
-        if(areTheyEqual)
-        {
+        if (areTheyEqual) {
             return false;
         }
-        if(!compareNode.contains(relatedTarget)){
+        if (!compareNode.contains(relatedTarget)) {
             fn();
         }
-    }); 
+    });
 }
 
 
 
 //Test building Table of Contents
 window.tocTest = tocTest;
-function tocTest(){
+function tocTest() {
     var chapter = new OrsChapter(813);
-    chapter.load().then(function () { 
-    chapter.injectAnchors();
-    let toc = chapter.buildToC();
-    modal.show();
-    modal.toc(toc);
-    modal.renderHtml(chapter.toString(), "ors-statutes");
+    chapter.load().then(function () {
+        chapter.injectAnchors();
+        let toc = chapter.buildToC();
+        modal.show();
+        modal.toc(toc);
+        modal.renderHtml(chapter.toString(), "ors-statutes");
 
-    window.location.hash = section; 
+        window.location.hash = section;
     });
 }
 
+//Test building of Volumes
 window.volTest = volTest;
-function volTest(){
+function volTest() {
     var chapter = new OrsChapter(813);
-    chapter.load().then(function () { 
+    chapter.load().then(function () {
         chapter.injectAnchors();
         let toc = chapter.buildToC();
         let vols = chapter.buildVolumes();
@@ -231,10 +207,17 @@ function volTest(){
         modal.titleBar(vols);
         modal.renderHtml(chapter.toString(), "ors-statutes");
         modal.titleBar(vols);
-        
-    
-        window.location.hash = section; 
-        });
-    
-    
+        window.location.hash = section;
+    });
 }
+
+//Test event handling on dropdown volume selector 
+window.dropdownTest = dropdownTest;
+function dropdownTest(){
+    var selector = document.getElementById("dropdown");
+    selector.addEventListener('change', function(){
+        
+    });
+}
+
+
