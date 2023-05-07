417832fb5fbeabe110186e8a18843374399b7a21
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-05-07 13:10:37 -0700

    Add keyup event handler.

--- dev_modules/webc-autocomplete/autocomplete.js
+++ dev_modules/webc-autocomplete/autocomplete.js
@@ -1,8 +1,7 @@
 
 
-import ReadingContext from "./reading-context/reading-context.js";
-import SearchClient from "./search-client/search-client.js";
-import "./html/html.js";
+import ReadingContext from "../reading-context/reading-context.js";
+import "../html/html.js";
 
 // Create a class for the element
 class Autocomplete extends HTMLElement {
@@ -23,10 +22,11 @@ class Autocomplete extends HTMLElement {
     
         // Create text node and add word count to it
         const text = document.createElement('span');
-        text.textContent = count;
+        text.setAttribute("id", "result")
+        text.textContent = "Some content here that used to be word count...";
     
         // Append it to the shadow root
-        shadow.appendChild(text);
+        // shadow.appendChild(text);
 
         // Create some CSS to apply to the shadow DOM
         const style = document.createElement("style");
@@ -34,16 +34,16 @@ class Autocomplete extends HTMLElement {
         /* CSS truncated for brevity */
         }`;
         
-        document.getElementById("q").addEventListener("keyup",suggest);
+        document.getElementById("q").addEventListener("keyup",this);
         // attach the created elements to the shadow DOM
-        this.shadowRoot.append(style, wrapper);
+        this.shadowRoot.append(style, text);
     }
 
     // Show suggestion based on user input.
-    suggest(e) {
+    handleEvent(e) {
         let target = e.target;
-        let suggestions = client.suggest(target.value);
-        render(suggestions);
+        let suggestions = this.src.suggest(target.value);
+        this.render(suggestions);
     }
 
 
@@ -55,8 +55,13 @@ class Autocomplete extends HTMLElement {
         
         console.log(word);
         
-        let suggestions = client.suggest(word);
+        let suggestions = this.src.suggest(word);
         console.log(suggestions);
+    }
+
+
+    source(src) {
+        this.src = src;
     }
 
 
@@ -86,40 +91,7 @@ class Autocomplete extends HTMLElement {
 
 
 
-class WordCount extends HTMLParagraphElement {
-    constructor() {
-        // Always call super first in constructor
-        super();
-    
-        // count words in element's parent element
-        // const wcParent = this.parentNode;
-        const wcParent = document.getElementById("q");
-        function countWords(node){
-            // const text = node.innerText || node.textContent;
-            const text = node.value; 
-            //             let re = /[\s\t\n\.]+/gmis;                       
-            return text.trim().split(/\s+/g).filter(a => a.trim().length > 0).length;
-        }
-    
-        const count = `Words: ${countWords(wcParent)}`;
-    
-        // Create a shadow root
-        const shadow = this.attachShadow({mode: 'open'});
-    
-        // Create text node and add word count to it
-        const text = document.createElement('span');
-        text.textContent = count;
-    
-        // Append it to the shadow root
-        shadow.appendChild(text);
-    
-        // Update count when element content changes
-        setInterval(function() {
-            const count = `Words: ${countWords(wcParent)}`;
-            text.textContent = count;
-        }, 200);
-    }
-}
+
 
 
 
