c7a37b4b54f65836f95c2e7acf167ec801a7fa0e
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-07-14 09:08:20 -0700

    Keep important directories.

---
+++ sites/sites-example.php
@@ -0,0 +1,18 @@
+<?php
+
+global $hostdata;
+
+$hostdata = array(
+    "default" => array(
+        "theme" => "biere-library"
+    ),
+    "thebierelibrary.com" => array(
+        "theme" => "biere-library"
+    ),
+    "appdev.ocdla.org" => array(
+        "theme" => "books-online"
+    ),
+    "ocdla.app" => array(
+        "theme" => "books-online"
+    )
+);
