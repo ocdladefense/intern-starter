70ec8289acce8b2a1a6d2a2de2977a9ea56b3c70
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-10-25 20:29:25 -0700

    Introduce routing separate from theme.

--- index.php
+++ index.php
@@ -5,9 +5,22 @@ require "bootstrap.php";
 
 loadEnv();
 
-$route = getRoute();
+// $route = getRoute();
+// $out = render($route);
+$req = getRequest();
 
-$out = render($route);
+$default_callback = function($req) {
+    return render($req);
+};
+
+
+$router = new Router();
+$router->addRoute(new WildCardRoute($default_callback));
+$route = $router->getRouteMatchingRequest($req);
+$callback = $route->getCallback();
+
+
+$out = call_user_func("render",$req->path);
 
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
