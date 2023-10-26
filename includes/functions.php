f434a824d2704e60879c52977aba15400fa95280
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-10-25 20:26:56 -0700

    Add new classes.

--- includes/functions.php
+++ includes/functions.php
@@ -22,7 +22,8 @@ function getTitle($route = null) {
         return $NO_TITLE;
     }
 
-    if(null == $meta[$route]) {
+
+    if(!isset($meta[$route])) {
         return $NO_TITLE;
     }
 
@@ -85,13 +86,100 @@ function loadEnv() {
 }
 
 
+// A wildcard route.
+function getRequest() {
+    $requestUri = $_SERVER["REQUEST_URI"];
+
+
+    $requestPath = explode("?",$requestUri)[0];
+    $basePath = substr($_SERVER["SCRIPT_NAME"],0,strlen($_SERVER["SCRIPT_NAME"])-9);
+    $length = strlen($basePath);
+    $path = substr($requestPath,$length);
+
+    $req = new stdClass();
+    $req->path = $path;
+
+    return $req;
+}
+
 
 function getRoute() {
     $requestUri = $_SERVER["REQUEST_URI"];
+
+
     $requestPath = explode("?",$requestUri)[0];
     $basePath = substr($_SERVER["SCRIPT_NAME"],0,strlen($_SERVER["SCRIPT_NAME"])-9);
     $length = strlen($basePath);
     $route = substr($requestPath,$length);
 
     return $route;
+}
+
+
+
+
+
+
+
+class Router {
+
+
+    private $routes = array();
+
+
+    public function __construct() {
+        $this->routes = array_map(function($item) {
+            return new Route($item);
+        },["/event/%"]);
+
+        array_unshift($this->routes, new WildCardRoute());
+    }
+
+
+    public function getRouteMatchingRequest($req) {
+        return $this->routes[0];
+    }
+
+    public function addRoute(Route $route) {
+        array_unshift($this->routes, $route);
+    }
+
+
+}
+
+
+
+class WildCardRoute extends Route {
+
+
+    public function __construct($callback = null) {
+        $this->pattern = "*";
+        $this->callback = $callback ??  function($req){return "The default route callback.";};
+    }
+
+
+    public function matches($req) {
+        return true;
+    }
+
+
+}
+
+
+
+class Route {
+
+    protected $pattern;
+
+    protected $callback;
+
+
+
+    public function matches($req) {
+        return false;
+    }
+
+    public function getCallback() {
+        return $this->callback;
+    }
 }
